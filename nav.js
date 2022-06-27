b2eea3253b67e6cea0a08a4c291f72703ddbc45f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-27 16:55:40 -0700

    Javascript works

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 //Credits: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
 
-fetch('nav.html')
+fetch('https://liljakiiski.github.io/nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
