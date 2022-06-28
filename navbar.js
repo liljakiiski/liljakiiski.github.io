fba8e8f2da7e9975b3f76fd3300a2bd69e1e8aa6
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-27 17:16:27 -0700

    Made assets directory

--- navbar.js
+++ navbar.js
@@ -1,6 +1,4 @@
-//Credits: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
-
-fetch('https://liljakiiski.github.io/navbar.html')
+fetch('https://liljakiiski.github.io/navbar.html') //Change to navbar.html once finished!
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#navbar");
