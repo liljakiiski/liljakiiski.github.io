e52e4addc784651ad1ec833badfe64aa8944b596
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-27 17:05:13 -0700

    Added navbar to all pages

--- navbar.js
+++ navbar.js
@@ -1,9 +1,9 @@
 //Credits: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
 
-fetch('https://liljakiiski.github.io/nav.html')
+fetch('https://liljakiiski.github.io/navbar.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
+    let oldelem = document.querySelector("script#navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
