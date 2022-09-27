70aa352a953a6670bfd125cf65f3e029f86dcad8
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-09-27 11:26:31 -0700

    Bunch of stuff removed, making this one page website

--- navbar.js
+++
@@ -1,9 +0,0 @@
-//fetch('navbar.html')
-fetch('https://liljakiiski.github.io/navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem, oldelem);
-})
