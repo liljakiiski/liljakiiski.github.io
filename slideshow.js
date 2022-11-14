3615cf73191d414ed99923ffe05410d219fc0022
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-11-13 21:13:34 -0800

    test again on mobile

--- slideshow.js
+++ slideshow.js
@@ -24,6 +24,6 @@ function showSlides(n) {
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-
+    
     slides[slideIndex-1].style.display = "block";  
 }
