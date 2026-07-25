7f61da15b51d8b3503fe72fb4c157a5a6db9e2b9
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2026-07-26 00:22:21 +0300

    pretty decent navbar + collapsible

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,43 @@
-var hamburger = document.querySelector('.hamburger');
-var navLinks = document.querySelector('.nav-links');
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
 
-hamburger.addEventListener('click', function () {
-  navLinks.style.display = navLinks.style.display === 'none' ? 'flex' : 'none';
-});
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+  const isMobile = e.matches
+  console.log(isMobile)
+  if(isMobile){
+    navbar.setAttribute('inert', '')
+  }
+  else{
+    // desktop device
+    navbar.removeAttribute('inert')
+  }
+}
+
+function openSidebar(){
+  navbar.classList.add('show')
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
+}
+
+function closeSidebar(){
+  navbar.classList.remove('show')
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+  dropdown.classList.remove('open')
+}
+
+updateNavbar(media)
+
+const dropbtn = document.querySelector('.dropdown .dropbtn')
+const dropdown = document.querySelector('.dropdown')
+
+dropbtn.addEventListener('click', (e) => {
+  if (media.matches) {           // only intercept on mobile widths
+    e.preventDefault()
+    dropdown.classList.toggle('open')
+  }
+})
